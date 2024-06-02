811de275b8549e9fa55cc745e2d3b4354e33dbcb
Tawfiq <ahmed.tawffiiq@gmail.com>
2024-06-02 16:12:57 +0300

    imp estimateAffinePartial2D

--- app/src/main/java/com/example/arcoredemo/MainActivity.kt
+++ app/src/main/java/com/example/arcoredemo/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.arcoredemo
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.arcoredemo.arcore.ARCorePermission
 import com.example.arcoredemo.databinding.ActivityMainBinding
 import com.example.arcoredemo.opencv.AppOpenCV
 import org.opencv.android.OpenCVLoader
+import org.opencv.core.MatOfPoint2f
+import org.opencv.core.Point
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var arCoreBackgroundSession = ARCoreBackgroundSession(this) { transition, direction ->
         movePinMarker(transition, direction)
+        estimateAffinePartial2D(transition)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,5 +50,23 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         arCoreBackgroundSession.start()
+    }
+
+    private fun estimateAffinePartial2D(transition: FloatArray) {
+        try {
+            val transitionX = transition[0].toDouble()
+            val transitionY = transition[1].toDouble()
+            val srcMatrix = MatOfPoint2f(Point(transitionX, transitionY))
+
+            val arrowX = binding.pinMarker.x.toDouble()
+            val arrowY = binding.pinMarker.y.toDouble()
+            val dstMatrix = MatOfPoint2f(Point(arrowX, arrowY))
+
+            val matrix = AppOpenCV.estimateAffinePartial2D(src = srcMatrix, dst = dstMatrix)
+            Log.d("AppOpenCV", "estimateAffinePartial2D: ${matrix.dump()}")
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            Log.d("AppOpenCV", "Exception: ${ex.message}")
+        }
     }
 }
--- app/src/main/java/com/example/arcoredemo/opencv/AppOpenCV.kt
+++ app/src/main/java/com/example/arcoredemo/opencv/AppOpenCV.kt
@@ -2,11 +2,37 @@ package com.example.arcoredemo.opencv
 
 import android.util.Log
 import org.opencv.android.OpenCVLoader
+import org.opencv.calib3d.Calib3d
+import org.opencv.core.Mat
+import org.opencv.core.Point
+import org.opencv.core.MatOfPoint2f
+import org.opencv.imgproc.Imgproc
 
 object AppOpenCV {
 
     fun setup() {
-        val isInitialized = OpenCVLoader.initLocal()
+        val isInitialized = OpenCVLoader.initDebug()
         Log.d("AppOpenCV", "OpenCVLoader: isInitialized $isInitialized")
+        debugEstimateAffinePartial2D()
+    }
+
+    private fun debugEstimateAffinePartial2D() {
+        val srcPoints = MatOfPoint2f(
+            Point(0.0, 0.0),
+            Point(1.0, 0.0),
+            Point(0.0, 1.0)
+        )
+        val dstPoints = MatOfPoint2f(
+            Point(0.0, 0.0),
+            Point(1.0, 1.0),
+            Point(1.0, 0.0)
+        )
+        estimateAffinePartial2D(src = srcPoints, dst = dstPoints)
+    }
+
+    fun estimateAffinePartial2D(src: MatOfPoint2f, dst: MatOfPoint2f): Mat {
+        val affineTransform = Calib3d.estimateAffinePartial2D(src, dst)
+        Log.d("AppOpenCV", "Affine Partial Transform Matrix: ${affineTransform.dump()}")
+        return affineTransform
     }
 }
